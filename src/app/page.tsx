f9b9004f707cb644f3a11603164fe3154f9416ef
Noor Karim <noorkarim777@gmail.com>
2025-08-30 14:45:11 +0600

    Remove SVG arrow from Book Your slot button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,12 +61,7 @@ export default function Home() {
               </li>
               <li>
                                   <NeumorphicButton href="https://tally.so/r/wz94LE">
-                    Book Your slot{" "}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '4px' }}>
-                      <path d="M13 10.9998L21.2 2.7998" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M22 6.8V2H17.2" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                      <path d="M11 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22H15C20 22 22 20 22 15V13" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                    </svg>
+                    Book Your slot
                   </NeumorphicButton>
               </li>
             </ul>
